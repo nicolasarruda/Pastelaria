68b03697d072872b5a835e2fd5201233fcd31358
Nicolas <nlarruda@hotmail.com>
2020-09-27 00:27:38 -0300

    Fazendo o arquivo, novos botoes e desconto

--- compra1.php
+++ compra1.php
@@ -15,7 +15,7 @@
 
 <center><h1>Dados da Compra</h1><br><br>
 
-<form name="cadastro" method="POST" action="">
+<form name="cadastro" method="POST" action="sucesso.php">
     <?php
     date_default_timezone_set('America/Sao_Paulo');
     $mes = date("m");
@@ -79,7 +79,7 @@ if($_POST[utf8_decode($aula -> codigo)]!=0){
       }
    }
    
-     if(isset($_POST['final'])){
+   if(isset($_POST['final'])){
    $num2 = $_POST['final'];
    echo "Preço da compra: R$ ".$num2;
    }
@@ -104,8 +104,17 @@ if($_POST[utf8_decode($aula -> codigo)]!=0){
     <label for="dinheiro" style="background-image:url(_imagens/dinheiro.jpg);"></label>
   </li>
 </ul>
+<br><br><br><br><br>
   
-<br><br><br><br><br><br><br>
+<h4>*Pagamento com dinheiro terá 5% desconto:</h4><br><br>
+<?php
+    if(isset($_POST['final'])){
+        $num2 = $_POST['final'];
+        echo "Preço da compra com desconto: R$ ".$num2 * 0.95;
+        }
+?>
+
+<br><br><br><br>
 
 
 <h3>Deseja receber nossa newsletter com promoções?</h3>
@@ -164,7 +173,107 @@ echo "Válido até: " . date(" d") . " de " . $mes . " de " . date(" Y ") . date
 ?>
 <br><br><br>
 
-<input type="submit" value="Gerar boleto" name="gerarboleto">
+
+<a href="cardapio.php"><input type="button" value="Alterar pedido"></a>
+<input type="submit" value="Imprimir pedido" name="imprimirpedido">
+
+
+
+<?php
+// Montando o arquivo.txt
+
+    date_default_timezone_set('America/Sao_Paulo');
+    $mes = date("m");
+    switch ($mes){
+        case '01':
+            $mes = 'janeiro';
+        break;
+        case '02':
+            $mes = 'fevereiro';
+        break;
+        case '03':
+            $mes = 'março';
+        break;
+        case '04':
+            $mes = 'abril';
+        break;
+        case '05':
+            $mes = 'maio';
+        break;
+        case '06':
+            $mes = 'junho';
+        break;
+        case '07':
+            $mes = 'julho';
+        break;
+        case '08':
+            $mes = 'agosto';
+        break;
+        case '09':
+            $mes = 'setembro';
+        break;
+        case '10':
+            $mes = 'outubro';
+        break;       
+        case '11':
+            $mes = 'novembro';
+        break;
+        case '12':
+            $mes = 'dezembro';
+        break;    
+    }
+
+    $horariodopedido = "Horário do pedido: " . date(" d") . " de " . $mes . " de " . date(" Y ") . date("H:i:s");
+    $horario = "23:59:59";
+    $horariovalidade = "Válido até: " . date(" d") . " de " . $mes . " de " . date(" Y ") . date($horario);
+
+    if(isset($_POST['nome']) == null){
+        $nome = "Não foi digitado nenhum nome";
+    } else {
+        $nome = $_POST['nome'];
+    }
+
+    $pastelselecionado = "Pastel selecionado(s):";
+
+    $link = "pasteis.xml";
+    $xml = simplexml_load_file($link) -> pasteis;
+
+    foreach($xml -> sabores as $aula){
+
+    if($_POST[utf8_decode($aula -> codigo)]){
+        $num1 = $_POST[utf8_decode($aula -> codigo)];
+        if ($num1 !=0){
+            
+        }
+        $num3 = ($aula -> preco);
+        $pastel = $num1 . "x Pastel (éis) do sabor:" .utf8_decode($aula -> nome). "<br>";
+        $pastelpreco = "Preço unitário: " . utf8_decode($num3) . "<br>";
+      }
+   }
+   
+   if(isset($_POST['final'])){
+   $num2 = $_POST['final'];
+   echo "Preço da compra: R$ ".$num2;
+   }
+
+
+    $fp = fopen("arquivo.txt", "w");
+    fwrite($fp,"Dados do pedido:");
+    fwrite('PHP_EOL', "\n");
+    fwrite($fp,$horariodopedido);
+    fwrite('PHP_EOL', "\n");
+    fwrite($fp,$horariovalidade);
+    fwrite('PHP_EOL', "\n");
+    fwrite($fp,$pastelselecionado);
+    fwrite('PHP_EOL', "\n");
+
+
+
+
+
+
+
+?>
 
 <br><br>
 
